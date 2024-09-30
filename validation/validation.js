99fef9d08451b1a75184b1012449f5471eebcb3e
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-09-30 21:42:50 +0300

    feat: end the form

---
+++ validation/validation.js
@@ -0,0 +1,45 @@
+export const validationRules = {
+    "email": {
+      "pattern": "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$",
+      "errorMessage": "Invalid email address"
+    },
+    "password": {
+      "minLength": 8,
+      "uppercase": "(?=.*[A-Z])",
+      "lowercase": "(?=.*[a-z])",
+      "number": "(?=.*\\d)",
+      "specialCharacter": "(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])",
+      "noSpace": "(?=\\s)",
+      "errorMessages": {
+        "minLength": "Password must be at least 8 characters long",
+        "uppercase": "Password must contain at least one uppercase letter",
+        "lowercase": "Password must contain at least one lowercase letter",
+        "number": "Password must contain at least one number",
+        "specialCharacter": "Password must contain at least one special character",
+        "noSpace": "Password cannot contain spaces",
+        "equalCurrent": "New password must be different from the current password",
+        "enterCurrent": "Enter your current password",
+        "confirm": "Passwords must match"
+      }
+    },
+    "firstName": {
+      "pattern": "^[a-zA-Z]+$",
+      "minLength": 1,
+      "firstCapital": true,
+      "errorMessages": {
+        "pattern": "Name must contain only english letters",
+        "minLength": "Name must be at least one character long",
+        "firstCapital": "Name must start with a capital letter"
+      }
+    },
+    "lastName": {
+    "pattern": "^[a-zA-Z]+$",
+      "minLength": 1,
+      "firstCapital": true,
+      "errorMessages": {
+        "pattern": "surname must contain only english letters",
+        "minLength": "surname must be at least one character long",
+        "firstCapital": "surname must start with a capital letter"
+      }
+    }
+  }
